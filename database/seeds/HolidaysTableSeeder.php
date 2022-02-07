466ed653a979b93fe7ccabc857142d7d3a6daad4
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-07 21:09:59 +0100

    Date formattate come si deve

--- database/seeds/HolidaysTableSeeder.php
+++ database/seeds/HolidaysTableSeeder.php
@@ -15,10 +15,15 @@ class HolidaysTableSeeder extends Seeder
     {
         $newHoliday = new Holiday();
         $newHoliday->tourist_package_name = $faker->sentence();
-        $newHoliday->from = $faker->county();
+        $newHoliday->from = $faker->country();
         $newHoliday->country_of_destination = $faker->countryISOAlpha3();
         $newHoliday->price = $faker->randomFloat(2, 80, 999);
-        $newHoliday->start_date = $faker->dateTimeThisYear('+2 months');
+        // -----------------------------------------------------------------------
+        // Una data compresa dalla prossima settimana fino ai prossimi 2 mesi
+        $dateWithHours = $faker->dateTimeBetween('+1 week', '+2 month');
+        // Formattato la data togliendo le ore, minuti e secondi
+        $newHoliday->start_date = $dateWithHours->format('Y-m-d');
+        // -----------------------------------------------------------------------
         $newHoliday->duration = $faker->numberBetween(3, 14);
         $newHoliday->description = $faker->paragraph();
         $newHoliday->save();
