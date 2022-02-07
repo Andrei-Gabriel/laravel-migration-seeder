d7ad747fd53d1bc128ec83153c7f38bd49568236
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-07 21:21:03 +0100

    Fine esercizio

--- database/seeds/HolidaysTableSeeder.php
+++ database/seeds/HolidaysTableSeeder.php
@@ -13,19 +13,27 @@ class HolidaysTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newHoliday = new Holiday();
-        $newHoliday->tourist_package_name = $faker->sentence();
-        $newHoliday->from = $faker->country();
-        $newHoliday->country_of_destination = $faker->countryISOAlpha3();
-        $newHoliday->price = $faker->randomFloat(2, 80, 999);
-        // -----------------------------------------------------------------------
-        // Una data compresa dalla prossima settimana fino ai prossimi 2 mesi
-        $dateWithHours = $faker->dateTimeBetween('+1 week', '+2 month');
-        // Formattato la data togliendo le ore, minuti e secondi
-        $newHoliday->start_date = $dateWithHours->format('Y-m-d');
-        // -----------------------------------------------------------------------
-        $newHoliday->duration = $faker->numberBetween(3, 14);
-        $newHoliday->description = $faker->paragraph();
-        $newHoliday->save();
+        for($i = 0; $i < 50; $i++) {
+            $newHoliday = new Holiday();
+            $newHoliday->tourist_package_name = $faker->sentence();
+            $newHoliday->from = $faker->country();
+            $newHoliday->country_of_destination = $faker->countryISOAlpha3();
+            $newHoliday->price = $faker->randomFloat(2, 80, 999);
+            // -----------------------------------------------------------------------
+            // Una data compresa dalla prossima settimana fino ai prossimi 2 mesi
+            $dateWithHours = $faker->dateTimeBetween('+1 week', '+2 month');
+            // Formattato la data togliendo le ore, minuti e secondi
+            $newHoliday->start_date = $dateWithHours->format('Y-m-d');
+            // -----------------------------------------------------------------------
+            /*****************************************************************************************************
+                Dovrebbe essere giusto, ma secondo me è una formattazione troppo restrittiva da fare in MySQL:
+                $daysWithoutDaySTR = $faker->numberBetween(3, 14);
+                $newHoliday->duration = $daysWithoutDaySTR += ' Days'; 
+            ******************************************************************************************************/
+            $newHoliday->duration = $faker->numberBetween(3, 14);
+            $newHoliday->description = $faker->paragraph();
+            $newHoliday->save();
+        }
+        
     }
 }
