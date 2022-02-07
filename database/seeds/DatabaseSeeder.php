4b74b8d0e21d454ed4fde5d2877e1a6604d494f3
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-07 20:51:17 +0100

    Corretto seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
-use App\Holiday;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,16 +9,8 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        $newHoliday = new Holiday();
-        $newHoliday->tourist_package_name = $faker->sentence();
-        $newHoliday->from = $faker->county();
-        $newHoliday->country_of_destination = $faker->countryISOAlpha3();
-        $newHoliday->price = $faker->randomFloat(2, 80, 999);
-        $newHoliday->start_date = $faker->dateTimeThisYear('+2 months');
-        $newHoliday->duration = $faker->numberBetween(3, 14);
-        $newHoliday->description = $faker->paragraph();
-        $newHoliday->save();
+        // $this->call(UsersTableSeeder::class);
     }
 }
