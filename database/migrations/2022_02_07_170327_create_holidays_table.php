5da1009cbfee4199226d5554d12114272770c436
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-07 19:35:52 +0100

    Migrazione delle colonne

--- database/migrations/2022_02_07_170327_create_holidays_table.php
+++ database/migrations/2022_02_07_170327_create_holidays_table.php
@@ -15,6 +15,11 @@ class CreateHolidaysTable extends Migration
     {
         Schema::create('holidays', function (Blueprint $table) {
             $table->id();
+            $table->string('tourist_package_name', 100);
+            $table->string('from', 100);
+            $table->string('destination', 100);
+            $table->float('price', 5, 2);
+            $table->tinyInteger('duration');
             $table->timestamps();
         });
     }
